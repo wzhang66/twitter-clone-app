c26abe18b8f60a8823e4b7e715b4f89b38d3a9bf
Weiwei <ww.zhang@mail.utoronto.ca>
2020-04-29 12:02:02 -0400

    set up the basic of the reducers

---
+++ src/store/reducers/authUser.js
@@ -0,0 +1,12 @@
+import {SET_AUTH_USER} from '../actions/authedUser';
+
+const authUser = (state=null,action) => {
+    switch(action.type) {
+        case SET_AUTH_USER: 
+            return action.id
+        default :
+            return state
+    }
+}
+
+export default authUser
